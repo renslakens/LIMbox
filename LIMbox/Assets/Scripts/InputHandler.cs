7fbe9d76bcc2a486354c5048f55cda26ceedf87e
Niekelodius <niekelodius@gmail.com>
2024-05-06 12:03:22 +0200

    fixed movement

--- LIMbox/Assets/Scripts/InputHandler.cs
+++ LIMbox/Assets/Scripts/InputHandler.cs
@@ -13,37 +13,49 @@ public class InputHandler : MonoBehaviour
 
   private bool isMoving = false;
 
+
   // Update is called once per frame
-private void Update() {
-  if (!isMoving) {
-    System.Func<KeyCode, bool> inputFunction;
-    if (isRepeatedMovement) {
-      inputFunction = Input.GetKey;
-    } else {
-      inputFunction = Input.GetKeyDown;
-    }
+  private void Update()
+  {
+    // Only process on move at a time.
+    if (!isMoving)
+    {
+      // Accomodate two different types of moving.
+      System.Func<KeyCode, bool> inputFunction;
+      if (isRepeatedMovement)
+      {
+        // GetKey repeatedly fires.
+        inputFunction = Input.GetKey;
+      }
+      else
+      {
+        // GetKeyDown fires once per keypress
+        inputFunction = Input.GetKeyDown;
+      }
 
-    Vector2 direction = Vector2.zero;
-    if (inputFunction(KeyCode.UpArrow)) {
-      direction = Vector2.up;
-    } else if (inputFunction(KeyCode.DownArrow)) {
-      direction = Vector2.down;
-    } else if (inputFunction(KeyCode.LeftArrow)) {
-      direction = Vector2.left;
-    } else if (inputFunction(KeyCode.RightArrow)) {
-      direction = Vector2.right;
-    }
-
-    // Check if there's a wall in the direction the player is trying to move
-    RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, gridSize);
-    if (hit.collider == null || hit.collider.isTrigger) {
-      StartCoroutine(Move(direction));
+      // If the input function is active, move in the appropriate direction.
+      if (inputFunction(KeyCode.UpArrow))
+      {
+        StartCoroutine(Move(Vector2.up));
+      }
+      else if (inputFunction(KeyCode.DownArrow))
+      {
+        StartCoroutine(Move(Vector2.down));
+      }
+      else if (inputFunction(KeyCode.LeftArrow))
+      {
+        StartCoroutine(Move(Vector2.left));
+      }
+      else if (inputFunction(KeyCode.RightArrow))
+      {
+        StartCoroutine(Move(Vector2.right));
+      }
     }
   }
-}
 
   // Smooth movement between grid positions.
-  private IEnumerator Move(Vector2 direction) {
+  private IEnumerator Move(Vector2 direction)
+  {
     // Record that we're moving so we don't accept more input.
     isMoving = true;
 
@@ -53,7 +65,8 @@ private void Update() {
 
     // Smoothly move in the desired direction taking the required time.
     float elapsedTime = 0;
-    while (elapsedTime < moveDuration) {
+    while (elapsedTime < moveDuration)
+    {
       elapsedTime += Time.deltaTime;
       float percent = elapsedTime / moveDuration;
       transform.position = Vector2.Lerp(startPosition, endPosition, percent);
@@ -63,7 +76,11 @@ private void Update() {
     // Make sure we end up exactly where we want.
     transform.position = endPosition;
 
+    // Wait for a slightly longer duration to ensure that the player can't move until the movement has completely finished.
+    yield return new WaitForSeconds(moveDuration + 0.07f);
+
     // We're no longer moving so we can accept another move input.
     isMoving = false;
   }
+
 }
